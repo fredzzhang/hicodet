160f9a4c1e0e313b30cf05708fc7bc807c7e4ec4
fred@macbook <frederic.zhang@anu.edu.au>
2020-12-10 13:41:18 +1100

    (#2) Specify an image using its index in the dataset

--- detections/visualise.py
+++ detections/visualise.py
@@ -14,18 +14,26 @@ from PIL import Image, ImageDraw
 
 import torch
 from torchvision.ops import nms
+from pocket.data import HICODet
 
 def visualize(args):
     # Set up root directory
     partition = args.partition
-    image_root = os.path.join(args.image_root, partition)
-    detection_root = os.path.join(args.detection_root, partition)
+    dataset = HICODet(None, os.path.join(
+        args.data_root, 'instances_{}.json'.format(args.partition)
+    ))
     # Set up image instance path
-    image_name = '{:08d}'.format(args.image_name)
-    image_path = os.path.join(image_root,
-            'HICO_{}_{}.jpg'.format(partition, image_name))
-    detection_path = os.path.join(detection_root,
-            'HICO_{}_{}.json'.format(partition, image_name))
+    image_name = dataset.filename(args.image_idx)
+    print("Image name: ", image_name)
+    image_path = os.path.join(
+        args.data_root,
+        'hico_20160224_det/images/{}'.format(args.partition),
+        image_name
+    )
+    detection_path = os.path.join(
+        args.detection_root,
+        image_name.replace('.jpg', '.json')
+    )
     # Load image instance
     image = Image.open(image_path)
     with open(detection_path, 'r') as f:
@@ -57,25 +65,12 @@ def visualize(args):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Visualize object detections")
-    parser.add_argument('--image-root',
-        type=str, 
-        default='../hico_20160224_det/images/')
-    parser.add_argument('--detection-root',
-        type=str, 
-        default='./')
-    parser.add_argument('--partition',
-        type=str, 
-        default='train2015')
-    parser.add_argument('--box-score-thresh',
-        type=float, 
-        default=0.2)
-    parser.add_argument('--nms-thresh',
-        type=float,
-        default=0.5)
-    parser.add_argument('--image-name',
-        help="Index appeared on an image file",
-        type=int, 
-        default=1)
+    parser.add_argument('--detection-root', type=str, required=True)
+    parser.add_argument('--image-idx', type=int, default=0)
+    parser.add_argument('--data-root', type=str, default='../')
+    parser.add_argument('--partition', type=str, default='train2015')
+    parser.add_argument('--box-score-thresh', type=float, default=0.2)
+    parser.add_argument('--nms-thresh', type=float, default=0.5)
     args = parser.parse_args()
 
     visualize(args)
