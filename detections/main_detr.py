2b1301b72c823c2ded238059a0836ee65689c8e3
fred@pegasus <frederic.zhang@anu.edu.au>
2021-10-18 14:45:06 +1100

    (#22) Revert to the default postprocessors
    Signed-off-by: fred@pegasus <frederic.zhang@anu.edu.au>

--- detections/main_detr.py
+++ detections/main_detr.py
@@ -133,31 +133,9 @@ class HICODetObject(Dataset):
         image, target = self.transforms(image, dict(boxes=boxes, labels=converted_labels))
         return image, target
 
-class PostProcess(torch.nn.Module):
-    @torch.no_grad()
-    def forward(self, outputs, target_sizes):
-        out_logits, out_bbox = outputs['pred_logits'], outputs['pred_boxes']
-
-        assert len(out_logits) == len(target_sizes)
-        assert target_sizes.shape[1] == 2
-
-        prob = F.softmax(out_logits, -1)
-        scores, labels = prob.max(-1)
-
-        # convert to [x0, y0, x1, y1] format
-        boxes = box_ops.box_cxcywh_to_xyxy(out_bbox)
-        # and from relative [0, 1] to absolute [0, height] coordinates
-        img_h, img_w = target_sizes.unbind(1)
-        scale_fct = torch.stack([img_w, img_h, img_w, img_h], dim=1)
-        boxes = boxes * scale_fct[:, None, :]
-
-        results = [{'scores': s, 'labels': l, 'boxes': b} for s, l, b in zip(scores, labels, boxes)]
-
-        return results
-
 def initialise(args):
     # Load model and loss function
-    detr, criterion, _ = build_model(args)
+    detr, criterion, postprocessors = build_model(args)
     if os.path.exists(args.pretrained):
         print(f"Load pre-trained model from {args.pretrained}")
         detr.load_state_dict(torch.load(args.pretrained)['model'])
@@ -209,7 +187,7 @@ def initialise(args):
         ), transforms
     )
 
-    return detr, criterion, PostProcess(), dataset
+    return detr, criterion, postprocessors, dataset
 
 def collate_fn(batch):
     images = []; targets = []
